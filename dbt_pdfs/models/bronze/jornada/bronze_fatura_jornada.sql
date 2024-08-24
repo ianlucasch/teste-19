3e1430075f0bf79b308474a339a4331ab6a65948
ianlucasch <ianlucaschaves@gmail.com>
2024-08-24 16:03:59 -0300

    Transformações das tabelas

--- dbt_pdfs/models/bronze/jornada/bronze_fatura_jornada.sql
+++ dbt_pdfs/models/bronze/jornada/bronze_fatura_jornada.sql
@@ -1,2 +1,18 @@
-SELECT *
-FROM {{ source ("automacao_pdfs", "fatura_jornada") }}
+WITH formatted AS (
+    SELECT
+        "n_nota" AS n_nota,
+        "cv",
+        "merc",
+        "tipo",
+        TO_DATE("vecto", 'DDMMYYYY') AS vecto,
+        CAST("qted" AS INT) AS qted,
+        "mercadoria",
+        CAST(REPLACE("cotacao", ',', '.') AS DECIMAL(10, 2)) AS cotacao,
+        TO_DATE("data_de_pregao", 'DDMMYYYY') AS data_de_pregao,
+        CAST(REPLACE("txop", ',', '.') AS DECIMAL(10, 2)) AS txop
+    FROM
+        {{ source('automacao_pdfs', 'fatura_jornada') }}
+)
+
+SELECT * 
+FROM formatted
